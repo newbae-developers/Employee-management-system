42f7dcbd6b21c50742fadec7c9c4f4404e4dcbdf
Samit khanal <50780984+samitkhanal@users.noreply.github.com>
2019-10-18 09:56:54 +0545

    Add files via upload

---
+++ Userform_login/Userform_login/Form1.Designer.vb
@@ -0,0 +1,164 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Login_btn = New System.Windows.Forms.Button()
+        Me.username11 = New System.Windows.Forms.TextBox()
+        Me.password11 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PictureBoxclose = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBoxclose, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Login_btn
+        '
+        Me.Login_btn.FlatAppearance.BorderSize = 0
+        Me.Login_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Login_btn.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Login_btn.Location = New System.Drawing.Point(206, 164)
+        Me.Login_btn.Name = "Login_btn"
+        Me.Login_btn.Size = New System.Drawing.Size(75, 23)
+        Me.Login_btn.TabIndex = 0
+        Me.Login_btn.Text = "Login"
+        Me.Login_btn.UseVisualStyleBackColor = True
+        '
+        'username11
+        '
+        Me.username11.Location = New System.Drawing.Point(164, 69)
+        Me.username11.Name = "username11"
+        Me.username11.Size = New System.Drawing.Size(167, 20)
+        Me.username11.TabIndex = 1
+        '
+        'password11
+        '
+        Me.password11.Location = New System.Drawing.Point(164, 116)
+        Me.password11.Name = "password11"
+        Me.password11.Size = New System.Drawing.Size(167, 20)
+        Me.password11.TabIndex = 2
+        Me.password11.UseSystemPasswordChar = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(66, 71)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 18)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Username"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(119, 186)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 13)
+        Me.Label2.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(66, 118)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(83, 18)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Password"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Tai Le", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(65, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(263, 21)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Attendance Management System"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Panel1.Controls.Add(Me.PictureBoxclose)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(418, 31)
+        Me.Panel1.TabIndex = 7
+        '
+        'PictureBoxclose
+        '
+        Me.PictureBoxclose.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBoxclose.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBoxclose.ErrorImage = Nothing
+        Me.PictureBoxclose.Image = CType(resources.GetObject("PictureBoxclose.Image"), System.Drawing.Image)
+        Me.PictureBoxclose.InitialImage = Nothing
+        Me.PictureBoxclose.Location = New System.Drawing.Point(382, -3)
+        Me.PictureBoxclose.Name = "PictureBoxclose"
+        Me.PictureBoxclose.Size = New System.Drawing.Size(36, 34)
+        Me.PictureBoxclose.TabIndex = 9
+        Me.PictureBoxclose.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange
+        Me.BackColor = System.Drawing.Color.Green
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(418, 221)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.password11)
+        Me.Controls.Add(Me.username11)
+        Me.Controls.Add(Me.Login_btn)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "loginform-view"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBoxclose, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Login_btn As Button
+    Friend WithEvents username11 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents password11 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBoxclose As PictureBox
+End Class
---
+++ Userform_login/Userform_login/Form1.vb
@@ -0,0 +1,54 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form1
+    Private rdr As Object
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles username11.TextChanged
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub Login_btn_Click(sender As Object, e As EventArgs) Handles Login_btn.Click
+        Dim constr = "Server=localhost;uid=root;database=user_databasefor_login_form"
+        Dim SQL = "Select * FROM login WHERE username1 = @username and password1 =@password"
+
+        Using dbcon As New MySqlConnection(constr)
+            Using Cmd As New MySqlCommand(SQL, dbcon)
+                dbcon.Open()
+                Cmd.Parameters.Add(New MySqlParameter("@username", username11.Text))
+                Cmd.Parameters.Add(New MySqlParameter("@password", password11.Text))
+                Using rdr = Cmd.ExecuteReader
+                    If rdr.HasRows Then
+                        MessageBox.Show("Login Successful")
+                        Form2.Show()
+                    Else
+                        MessageBox.Show("Username and password not found")
+                    End If
+
+                End Using
+                dbcon.Close()
+            End Using
+
+        End Using
+
+    End Sub
+
+    Private Sub Password_TextChanged(sender As Object, e As EventArgs) Handles password11.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles PictureBoxclose.Click
+        Me.Close()
+    End Sub
+End Class
